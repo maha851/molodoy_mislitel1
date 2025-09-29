fea3dd47fa1eeb7c1b7c5b1e334ba37a12914c21
ahma <ahmadbejtullaev@gmail.com>
2025-09-29 21:20:57 +0300

    в

--- database/database.py
+++ database/database.py
@@ -14,6 +14,7 @@ load_dotenv(find_dotenv())
 
 
 HidenCat=os.getenv('HidenCat')
+outh = os.getenv('')
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 CREDS = service_account.Credentials.from_service_account_file(HidenCat, scopes=SCOPES)
