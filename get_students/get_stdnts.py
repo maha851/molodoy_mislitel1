e927123ab815a305436361d654f3cf567e64f4f7
maha <ahmadbejtullaev@gmail.com>
2025-11-06 23:30:12 +0300

    Update get_stdnts.py

--- get_students/get_stdnts.py
+++ get_students/get_stdnts.py
@@ -44,7 +44,7 @@ def keyboard_from_students(frist_leters):
     builder.add(KeyboardButton(text="❌ Отмена"))
 
     # Форматируем в сетку 2 колонки
-    builder.adjust(3)
+    builder.adjust(2)
     return builder.as_markup(resize_keyboard=True)
 
 
